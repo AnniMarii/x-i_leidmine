f564607c13189944d8da410f85d52aee512dfe3d
AnniMarii <34130543+AnniMarii@users.noreply.github.com>
2017-12-12 09:37:53 +0200

    Add files via upload

---
+++ x_leidmin.py
@@ -0,0 +1,22 @@
+#proframm leiab tehtes x väärtuse, antud avaldise puhul
+import cmath
+print("Tere! Programm arvutab sulle ruutvõrrandi vastused!")
+print("Sisesta a:")
+a = int(input())
+print("Sisesta b:")
+b = int(input())
+print("Sisesta c:")
+c = int(input())
+#0 = a * x** + b * x + c
+x1 = (-b+ round((b**- 4*a*c)**(1/2)))/(2*a)
+x2 = (-b- round((b**- 4*a*c)**(1/2)))/(2*a)
+print(x1)
+print(x2) 
+#D =(b- 4*a*c)
+#if  D < 0 :
+   #print("Lahendid puuduvad")
+#else:
+    #print(x1)
+    #print(x2)
+
+
